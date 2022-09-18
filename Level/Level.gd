7cd03a1287723a38ae01cb2ce77f12e5df64e088
Sisipun <sisipun@gmail.com>
2022-09-18 14:24:34 +0300

    Generate aligned field from tiles

--- Level/Level.gd
+++ Level/Level.gd
@@ -5,8 +5,17 @@ class_name Level
 
 
 export (PackedScene) var _tile_scene = _tile_scene as PackedScene
+export (int) var size = 5
+
+
+var _map: Array = [] 
 
 
 func _ready() -> void:
-	var tile: Tile = _tile_scene.instance()
-	add_child(tile)
+	for i in range(size):
+		for j in range(size):
+			var tile: Tile = _tile_scene.instance()
+			var offset: float = (size - 1.0) / 2.0
+			tile.init(Vector3((i - offset) * tile.length(), 0, (j - offset) * tile.width()))
+			_map.append(tile)
+			add_child(tile)
