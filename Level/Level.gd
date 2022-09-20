c5d5ab97c6be9f51caa09d3df54ce99d14267e97
Sisipun <sisipun@gmail.com>
2022-09-20 19:04:15 +0300

    Show selected tile

--- Level/Level.gd
+++ Level/Level.gd
@@ -8,14 +8,29 @@ export (PackedScene) var _tile_scene = _tile_scene as PackedScene
 export (int) var size = 5
 
 
-var _map: Array = [] 
+var _map: Array = []
+var _selected_tile: Tile = null
 
 
 func _ready() -> void:
-	for i in range(size):
-		for j in range(size):
-			var tile: Tile = _tile_scene.instance()
-			add_child(tile)
-			_map.append(tile)
+	for x in range(size):
+		for y in range(size):
+			var tile: Tile = _add_tile()
 			var offset: float = (size - 1.0) / 2.0
-			tile.init(Vector3((i - offset) * tile.length(), 0, (j - offset) * tile.width()))
+			tile.init(x, y, Vector3((x - offset) * tile.length(), 0, (y - offset) * tile.width()))
+
+
+func _add_tile() -> Tile:
+	var tile: Tile = _tile_scene.instance()
+	add_child(tile)
+	_map.append(tile)
+	tile.connect("pressed", self, "_on_tile_pressed")
+	return tile
+
+
+func _on_tile_pressed(x, y) -> void:
+	if (_selected_tile):
+		_selected_tile.set_unclicked()
+	
+	_selected_tile = _map[x * size + y]
+	_selected_tile.set_clicked()
--- Level/Tile.gd
+++ Level/Tile.gd
@@ -4,20 +4,46 @@ extends StaticBody
 class_name Tile
 
 
+signal pressed(x, y)
+
+
 export (NodePath) onready var _shape = get_node(_shape) as CollisionShape
+export (NodePath) onready var _body = get_node(_body) as MeshInstance
+export (Material) var _material
+export (Material) var _clicked_material
 
 
-func init(position: Vector3) -> void:
+var _x = 0
+var _y = 0
+
+
+func init(x: int, y: int, position: Vector3) -> void:
 	self.transform.origin = position
+	self._x = x
+	self._y = y
+	self.set_unclicked()
 
 
 func length() -> float:
-	return _shape.shape.extents.x
+	return _shape.shape.extents.x * 2
 
 
 func width() -> float:
-	return _shape.shape.extents.y
+	return _shape.shape.extents.y * 2
 
 
 func height() -> float:
-	return _shape.shape.extents.z
+	return _shape.shape.extents.z * 2
+
+
+func set_clicked():
+	_body.set_material_override(_clicked_material)
+
+
+func set_unclicked():
+	_body.set_material_override(_material)
+
+
+func _on_event(camera: Node, event: InputEvent, position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_LEFT:
+		emit_signal("pressed", _x, _y)
