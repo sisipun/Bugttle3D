8f32e77285ddaf8570f39c021f2db3228d5b7226
sisipun <sisipun@gmail.com>
2022-10-11 19:28:50 +0300

    Show selected skill and change skill action

--- script/controller/user_controller.gd
+++ script/controller/user_controller.gd
@@ -2,6 +2,9 @@ class_name UserController
 extends BaseController
 
 
+export (NodePath) onready var _ui = get_node(_ui) as Ui
+
+
 var _selected_tile: Tile = null
 var _selected_bug: Bug = null
 var _selected_skill: Skill = null
@@ -17,6 +20,11 @@ func process_turn(_delta: float) -> void:
 		_cancel()
 	if Input.is_action_just_pressed("controller_end_turn"):
 		emit_signal("turn_ended")
+	if Input.is_action_just_pressed("controller_next_skill") and _selected_bug:
+		var skills: Array = _selected_bug.skills
+		var selected_skill_index: int = _selected_bug.skills.find(_selected_skill)
+		_select_skill(skills[selected_skill_index + 1 if selected_skill_index + 1 < len(skills) else 0])
+		
 
 
 func after_turn() -> void:
@@ -29,7 +37,7 @@ func _on_tile_pressed(tile: Tile) -> void:
 		_selected_tile.set_unclicked()
 	
 	if !_selected_tile or !_selected_bug or !_selected_skill:
-		_select(tile)
+		_select_tile(tile)
 		return
 	
 	if (
@@ -38,19 +46,29 @@ func _on_tile_pressed(tile: Tile) -> void:
 	):
 		_cancel()
 	else:
-		_select(tile)
+		_select_tile(tile)
 
 
 func _cancel() -> void:
 	if _selected_tile:
 		_selected_tile.set_unclicked()
 	
-	_select(null)
+	_select_tile(null)
 
 
-func _select(tile: Tile) -> void:
+func _select_tile(tile: Tile) -> void:
 	_selected_tile = tile
-	_selected_bug = tile.bug if tile else null
-	_selected_skill = _selected_bug.skills[0] if _selected_bug and _selected_bug.team == team else null
-	if tile:
+	_select_bug(_selected_tile.bug if _selected_tile else null)
+	
+	if _selected_tile:
 		_selected_tile.set_clicked()
+
+
+func _select_bug(bug: Bug) -> void:
+	_selected_bug = bug
+	_select_skill(_selected_bug.skills[0] if _selected_bug and _selected_bug.team == team else null)
+
+
+func _select_skill(skill: Skill) -> void:
+	_selected_skill = skill
+	_ui.set_skill_icon(_selected_skill.icon if _selected_skill else null)
--- script/ui/ui.gd
+++ script/ui/ui.gd
@@ -1,2 +1,9 @@
 class_name Ui
 extends Control
+
+
+export (NodePath) onready var _skill_icon = get_node(_skill_icon) as TextureRect
+
+
+func set_skill_icon(icon: Texture) -> void:
+	_skill_icon.texture = icon
